efa59150ba5cafdd80a184d34fc64e938363097f
Stepulik <stepan.satsuk@yandex.ru>
2022-03-09 22:02:31 +0300

    Добавьте файлы проекта.

---
+++ _153502_Сацюк_lab1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter 2 numbers");
+
+double a = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+double c = a / b;
+
+Console.WriteLine(c);
+
+Console.ReadKey();
