805081db11518a7e50c8a36c9ae54d736615952b
Stepulik <stepan.satsuk@yandex.ru>
2022-03-17 11:40:49 +0300

    qqq

---
+++ Laba_2/Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int a, b;
+a = num / 100;
+b = (num % 100 - num % 10) / 10;
+
+int c = (a > b) ? a : b;
+if (c == a)
+{
+    Console.WriteLine("Первое больше второго");
+}
+else
+    Console.WriteLine("Второе больше первого");
---
+++ Laba_2/Task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+bool is_true;
+do
+{
+    is_true = false;
+    int choise;
+    Console.WriteLine("Введите координаты точки по х:");
+    int x = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите координаты точки по у:");
+    int y = Convert.ToInt32(Console.ReadLine());
+
+    if(x > 40 || y > 40 || x < -40 || y < -40)
+        Console.WriteLine("Да");
+    else if (x == 40 || y == 40)
+        Console.WriteLine("На границе");
+    else
+        Console.WriteLine("Нет");
+
+    Console.WriteLine("Желаете продолжить? (1 - да, 0 - нет)");
+    choise = Convert.ToInt32(Console.ReadLine());
+    switch (choise)
+    {
+        case 0:
+            is_true = false;
+            break;
+        case 1:
+            is_true = true;
+            break;
+    }
+} while (is_true);
